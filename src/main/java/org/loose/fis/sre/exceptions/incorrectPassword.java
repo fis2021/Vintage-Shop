5d6bba007712e470a6fbb90c20401bd90e12a1ae
PaulaB27 <80637779+PaulaB27@users.noreply.github.com>
2021-05-13 23:59:24 +0300

    VS-1 login implemented feature

---
+++ src/main/java/org/loose/fis/sre/exceptions/incorrectPassword.java
@@ -0,0 +1,8 @@
+package org.loose.fis.sre.exceptions;
+
+public class incorrectPassword extends Exception{
+    public incorrectPassword(String s)
+    {
+        super(s);
+    }
+}
---
+++ src/main/java/org/loose/fis/sre/exceptions/invalidUsername.java
@@ -0,0 +1,13 @@
+package org.loose.fis.sre.exceptions;
+
+import javafx.scene.text.Text;
+
+public class invalidUsername extends Exception {
+
+
+    public invalidUsername(String s) {
+        super(s);
+
+    }
+
+}
